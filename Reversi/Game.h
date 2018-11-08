c1e600e72744ed4bc43a8fa72201ecb0fd7e561c
Magic Mo <Kyoya@live.ca>
2018-11-08 12:14:43 -0500

    A bit of the game header.

--- Reversi/Game.h
+++ Reversi/Game.h
@@ -1,4 +1,14 @@
 #ifndef GAME_HEADER
 #define GAME_HEADER
-
+#include "Board.h"
+#include "Player.h"
+class Game {
+public:
+	Game();
+	~Game();
+	void selectPlayers();
+	Player* nextPlayer();
+	void play();
+	void announceWinner();
+};
 #endif
