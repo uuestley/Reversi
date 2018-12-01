04c65e936c2dd32984f084681bf31b01c5b0be53
Magic Mo <Kyoya@live.ca>
2018-11-30 22:44:35 -0500

    Minor additions

--- Reversi/Game.cpp
+++ Reversi/Game.cpp
@@ -1,6 +1,11 @@
 #include "Game.h"
-
+#include <iostream>
+using namespace std;
 class Game {
+public:
+	Board game;
+	Player* player1;
+	Player* player2;
 	Game() {
 
 	}
@@ -10,7 +15,18 @@ class Game {
 	}
 
 	void selectPlayers() {
-
+		int type1;
+		int type2;
+		cout << "Is Player 1 human or computer? (reply '1' for human, reply '2' for computer)";
+		cin >> type1;
+		cout << "Is Player 2 human or computer? (reply '1' for human, reply '2' for computer)";
+		cin >> type2;
+		/*
+		switch (type1) {
+		case 1:
+			break;
+		}
+		*/
 	}
 
 	Player* nextPlayer() const {
@@ -18,10 +34,19 @@ class Game {
 	}
 
 	void play() {
-
+		/*
+		int temp;
+		while (isRunning) {
+			cin >> temp;
+			Board::makeMove(Piece::LIGHT, temp);
+		}
+		*/
 	}
 
 	void announceWinner() {
+
+	}
+	bool isRunning() {
 
 	}
 };
